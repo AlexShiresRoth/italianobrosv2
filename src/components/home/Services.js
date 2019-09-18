60280341a28b5145496e5d21e23b3a90c51543cf
Alex Rothenberg <alexroth96@gmail.com>
2019-09-17 22:17:41 -0400

    refactor services section on home

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react"
+import { Link } from "gatsby"
+
+import layoutStyles from "./servicestyles/Services.module.scss"
 
 const Services = () => {
-    return (
-        <div>
-            
+  const servicesArray = [
+    { title: "custom architectural concrete", img: "c1" },
+    { title: "venetian plaster", img: "c2" },
+    { title: "stucco", img: "c3" },
+    { title: "dryWall", img: "c4" },
+  ]
+
+  return (
+    <section className={layoutStyles.services__section}>
+      <div className={layoutStyles.container}>
+        <div className={layoutStyles.heading}>
+          <h3 className={layoutStyles.services__h3}>
+            Professionally Handcrafted
+          </h3>
+          <hr className={layoutStyles.services__hr} />
         </div>
-    )
+        <div className={layoutStyles.paragraph__section}>
+          <p className={layoutStyles.paragraph}>
+            Our experts are highly skilled in services for high end residential,
+            large volume residential, as well as commercial applications of
+            sheetrock, plaster, exterior stucco systems and more.
+          </p>
+        </div>
+        <div className={layoutStyles.button__container}>
+          <Link
+            to={"/Services"}
+            style={{ textDecoration: "none", alignSelf: "center" }}
+          >
+            <button className={layoutStyles.button}>Learn More</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Services
