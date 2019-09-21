ca3fd98ef62b43226b17e734dc7dc71a641400e0
Alex Rothenberg <alexroth96@gmail.com>
2019-09-21 13:44:59 -0400

    finish contact page

--- src/components/location/LocationInfo.js
+++ src/components/location/LocationInfo.js
@@ -1,28 +1,28 @@
 import React from "react"
 
+import layoutStyles from "./styles/LocationInfo.module.scss"
+
 const LocationInfo = () => {
   return (
-    <section className="address__container">
-      <div className="mobile__heading-center">
+    <section className={layoutStyles.address__section}>
+      <div className={layoutStyles.heading}>
         <h2>Contact Us</h2>
-        <div className="paragraph__section--center">
-          <p>Looking to schedule an appointment? Have any questions?</p>
-        </div>
+        <p>Looking to schedule an appointment? Have any questions?</p>
       </div>
-      <div className="address__container--address">
-        <div className="address__container--address-column">
+      <div className={layoutStyles.address__container}>
+        <div className={layoutStyles.address__column}>
           <h3>Address</h3>
           <hr />
           <p>20 Jules CT. Bohemia, NY</p>
         </div>
-        <div className="address__container--address-column">
+        <div className={layoutStyles.address__column}>
           <h3>Phone</h3>
           <hr />
           <p>
             <a href="tel:+1-631.218.3641">631.218.3641</a>
           </p>
         </div>
-        <div className="address__container--address-column">
+        <div className={layoutStyles.address__column}>
           <h3>Email</h3>
           <hr />
           <p>
